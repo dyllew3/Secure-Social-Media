82353112bf4010a1ef464a328cb113456f63e681
dyllew3 <digitald83@gmail.com>
2018-03-27 23:40:16 +0100

    Got encryption and decryption working

--- Server/server.js
+++ Server/server.js
@@ -2,8 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 const cryptico = require('cryptico');
-const atob = require('atob').atob;
-const btoa = require('btoa');
+const CryptoJS = require("crypto-js");
 
 const Bits = 2048;
 const PassPhrase = "Veteran of a thousand psychic wars";
@@ -41,7 +40,7 @@ function getUrlVars(url) {
 server = http.createServer( function(req, res) {
 
     //console.dir(req.param);
-	
+
     if (req.method == 'POST') {
         console.log("POST");
         var body = '';
@@ -49,12 +48,12 @@ server = http.createServer( function(req, res) {
             body += data;
         });
         req.on('end', function () {
-			
+
 			var newDict = getUrlVars(body);
 			console.log(body);
 			console.log(newDict['action'] + " ");
 			var responseText = "Invalid request";
-			
+
 			if ("action" in newDict){
 				if (newDict['action'] == SendPubKey){
 					responseText =  cryptico.publicKeyString(RSAKey);
@@ -78,7 +77,7 @@ server = http.createServer( function(req, res) {
 
 					users = {};
 					users[newDict['user']] = {'role':'admin'};
-					
+
 					admin = users[newDict['user']];
 
 					admin['pubKey'] = newDict['pubKey'];
@@ -89,9 +88,10 @@ server = http.createServer( function(req, res) {
 					responseText = "Created group";
 				}
 				else if (newDict['action'] == Encrypt && 'user' in newDict && 'group' in newDict && 'text' in newDict){
-					var group = newDict['group']; 
+
+          var group = newDict['group'];
 					if(group in Groups && newDict['user'] in Groups[group]['users']){
-						var str = cryptico.decrypt(btoa(newDict['text']), RSAKey);
+						var str = cryptico.decrypt(newDict['text'], RSAKey);
 						var AESKey = Groups[group]['key'];
 						var result = cryptico.encryptAESCBC(str, AESKey);
 						responseText = result;
@@ -106,12 +106,12 @@ server = http.createServer( function(req, res) {
 					var user = newDict['user'];
 					if(group in Groups && newDict['user'] in Groups[group]){
 
-						var str = newDict['text'];
+						var str = decodeURIComponent(newDict['text']);
 						var AESKey = Groups[group]['key'];
 						var pubKey = Groups[group][user]['key'];
 						var result = cryptico.decryptAESCBC(str, AESKey);
 
-						result = cryptico.encrypt(result, pubKey);
+						result = cryptico.encrypt(result, pubKey).cipher;
 						responseText = result;
 					}
 					else {
@@ -130,7 +130,7 @@ server = http.createServer( function(req, res) {
     {
         console.log("GET");
 		var responseText = "Invalid Action";
-		
+
 		var body = '';
         var newDict = url.parse(req.url, true).query;
 		console.log(newDict);
@@ -158,24 +158,26 @@ server = http.createServer( function(req, res) {
 
 				users = {};
 				users[newDict['user']] = {'role':'admin'};
-				
+
 				admin = users[newDict['user']];
 
 				admin['key'] = newDict['pubKey'];
 				Groups[newDict['group']] = {};
 				Groups[newDict['group']]['users'] = users;
-				var key = cryptico.generateAESKey();
+				var key = new Buffer(cryptico.generateAESKey());
+        key = key.toString();
 				Groups[newDict['group']]['key'] = key;
 				responseText = "Created group";
 			}
 			else if (newDict['action'] == Encrypt && 'user' in newDict && 'group' in newDict && 'text' in newDict){
-				var group = newDict['group']; 
+				var group = newDict['group'];
 				if(group in Groups && newDict['user'] in Groups[group]['users']){
-					var str = cryptico.decrypt(decodeURI(newDict['text']), RSAKey);
-					console.log("string is " + typeof(str.plaintext));
-					var AESKey = Groups[group]['key'];
-					var result = cryptico.encryptAESCBC(str, AESKey);
-					responseText = result;
+					var str = cryptico.decrypt(newDict['text'], RSAKey).plaintext;
+					console.log("string is " + str);
+					var key = Groups[group]['key'];
+					var result = CryptoJS.AES.encrypt(str, key).toString();
+          console.log(result);
+          responseText = result;
 				}
 				else {
 					console.log(" group details");
@@ -190,9 +192,9 @@ server = http.createServer( function(req, res) {
 					var str = newDict['text'];
 					console.log(str);
 					var AESKey = Groups[group]['key'];
-					
+
 					var pubKey = Groups[group]['users'][user]['key'];
-					var result = cryptico.decryptAESCBC(str, AESKey);
+					var result = CryptoJS.AES.decrypt(str, AESKey).toString(CryptoJS.enc.Utf8);
 					console.log(result);
 					//result = cryptico.encrypt(result, pubKey).cipher;
 					responseText = result;
